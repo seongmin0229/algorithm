209d8d0335d4592989dcc5d60ff08d02b203ac52
이성민 <114327218+seongmin0229@users.noreply.github.com>
2025-05-13 20:45:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 556 ms, Memory: 143692 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,13 +1,11 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
-    static Map<Integer, List<Integer>> map;
-    static boolean[] visit;
+    // union-find 풀이
+
+    static int[] parent;
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,44 +15,46 @@ public class Main {
         int n = Integer.parseInt(split[0]);
         int m = Integer.parseInt(split[1]);
 
-        map = new HashMap<>();
-        visit = new boolean[n];
+        parent = new int[n + 1];
 
-        for(int i = 0; i < n; i++){
-            map.put(i + 1, new ArrayList<>());
+        // 초기화
+        for(int i = 1; i < n + 1; i++){
+            parent[i] = i;
         }
 
         for(int i = 0; i < m; i++){
-            String[] tmpSplit = br.readLine().split(" ");
-            int a = Integer.parseInt(tmpSplit[0]);
-            int b = Integer.parseInt(tmpSplit[1]);
+            String[] tmp = br.readLine().split(" ");
+            int a = Integer.parseInt(tmp[0]);
+            int b = Integer.parseInt(tmp[1]);
 
-            List<Integer> list = map.get(a);
-            list.add(b);
-
-            List<Integer> list2 = map.get(b);
-            list2.add(a);
+            union(a, b);
         }
 
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(!visit[i]){
-                count++;
-                dfs(i);
-            }
+        Set<Integer> components = new HashSet<>();
+        for(int i = 1; i < n + 1; i++){
+            components.add(find(i));
         }
-        bw.append(String.valueOf(count));
+
+        bw.append(String.valueOf(components.size()));
+
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static void dfs(int i){
-        visit[i] = true;
-        List<Integer> list = map.get(i + 1);
-        for (Integer integer : list) {
-            if(!visit[integer - 1])
-                dfs(integer - 1);
+    static int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    static void union(int a, int b){
+        int pa = find(a);
+        int pb = find(b);
+
+        if(pa != pb){
+            parent[pa] = pb;
         }
     }
 }
