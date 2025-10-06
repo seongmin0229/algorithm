0e3fefb2549c3350419b70a9deaa2bafd2289d38
이성민 <114327218+seongmin0229@users.noreply.github.com>
2025-10-06 19:24:35 +0900

    [Gold IV] Title: 여행 가자, Time: 152 ms, Memory: 16904 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,67 +1,62 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
     static int[] parent;
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
 
         parent = new int[n];
+        for(int i = 0; i < n; i++) parent[i] = i;
 
+
+        StringTokenizer st;
         for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-
-        for(int i = 0; i < n; i++){
-            String[] line = br.readLine().split(" ");
-
-            for(int j = 0; j < line.length; j++){
-                if(Integer.parseInt(line[j]) == 1){
-                    union(i, j);
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++){
+                int isConnected = Integer.parseInt(st.nextToken());
+                if(isConnected == 1){
+                    if(find(i) != find(j)) union(i, j);
                 }
             }
         }
 
-        String[] line = br.readLine().split(" ");
-
-        int root = find(Integer.parseInt(line[0]) - 1);
-        boolean flag = true;
-
-        for(int i = 1; i < m; i++){
-            if(find(Integer.parseInt(line[i]) - 1) != root){
-                flag = false;
-                break;
-            }
-        }
-
-        if(flag) bw.append("YES");
-        else bw.append("NO");
-
 //        System.out.println(Arrays.toString(parent));
 
-        bw.flush();
-        bw.close();
-        br.close();
+        st = new StringTokenizer(br.readLine());
+
+        int first = Integer.parseInt(st.nextToken()) - 1;
+        while(st.hasMoreTokens()){
+            int cur = Integer.parseInt(st.nextToken()) - 1;
+            if(parent[cur] != parent[first]) {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println("YES");
+
+
     }
 
-    private static int find(int x) {
-        if(x != parent[x]){
-            return find(parent[x]);
+    static int find(int x){
+        if(x == parent[x]) return x;
+        else{
+            int root = find(parent[x]);
+            parent[x] = root;
+            return root;
         }
-        return parent[x];
     }
 
-    private static void union(int a, int b){
-        int rootA = find(a);
-        int rootB = find(b);
-        if(rootA != rootB){
-            parent[rootB] = rootA;
-        }
+    static void union(int a, int b){
+        int aParent = find(a);
+        int bParent = find(b);
+
+        if(aParent < bParent) parent[bParent] = aParent;
+        else parent[aParent] = bParent;
     }
 }
